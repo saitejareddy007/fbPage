f406274ef5cba945f1671fbd1396e7ee97a0f010
saitejareddy007 <tejareddy.d321@gmail.com>
2017-06-16 22:52:38 +0530

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -1,39 +1,42 @@
 <?php
-
-$con=mysqli_connect("localhost","root","sai","chintu");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-	$fname=$_POST['fname'];
-	$surname=$_POST['Surname'];
-	$MorE=$_POST['MorE'];
-	$newpassword=$_POST['new_password'];
-	$reenter=$_POST['Re-enter_new_password'];
-	if($fname==""||$surname==""||$MorE==""||$newpassword==""||$reenter==""){
-?>
-		<script type="text/javascript">
-			alert("please fill all the fields.");
-			window.location="facebook.htm";
-		</script>
-<?php
+	$con=mysqli_connect("localhost","root","sai","chintu");
+	// Check connection
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-	if($newpassword!=$reenter){
-?>
-		<script>
-			alert("password mismatched");
-			window.location="facebook.htm";
-		</script>
-<?php
+	else{
+		$fname=$_POST['fname'];
+		$surname=$_POST['Surname'];
+		$MorE=$_POST['MorE'];
+		$newpassword=$_POST['new_password'];
+		$reenter=$_POST['Re-enter_new_password'];
+		if($fname==""||$surname==""||$MorE==""||$newpassword==""||$reenter==""){
+			?>
+				<script type="text/javascript">
+					alert("please fill all the fields.");
+					window.location="facebook.htm";
+				</script>
+			<?php
+		}
+		if($newpassword!=$reenter){
+			?>
+				<script>
+					alert("password mismatched");
+					window.location="facebook.htm";
+				</script>
+			<?php
 
-	}
+		}
 
-	if(!mysqli_query($con,"INSERT INTO account (firstname,surname,mobile_or_email,new_password,reenter_password) VALUES ('$fname','$surname','$MorE','$newpassword','$reenter')")){
-		echo("Error description: " . mysqli_error($con));
+		if(!mysqli_query($con,"INSERT INTO account (firstname,surname,mobile_or_email,new_password,reenter_password) VALUES ('$fname','$surname','$MorE','$newpassword','$reenter')")){
+			echo("Error description: " . mysqli_error($con));
+		}
+		else{
+			echo "you account has been created successfully";
+			//($_POST['fname'],$_POST['surname'],$_POST['MorE'],$_POST['new_password'],$_POST['Re-enter_new_password'])
+			//'fne','sume','MorE','newword','Re-erd')
+		}
 	}
-	echo "you account has been created successfully";
-	//($_POST['fname'],$_POST['surname'],$_POST['MorE'],$_POST['new_password'],$_POST['Re-enter_new_password'])
-	//'fne','sume','MorE','newword','Re-erd')
 ?>
 <a href="facebook.htm">Click here Login<a/>
