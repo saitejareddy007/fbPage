3aa4101c7ad9b679c263c2fb49796e9c0d32b3ce
saitejareddy007 <tejareddy.d321@gmail.com>
2018-01-06 16:31:32 +0530

    basic update

--- create_account.php
+++ create_account.php
@@ -1,42 +1,27 @@
 <?php
-	$con=mysqli_connect("localhost","root","sai","chintu");
-	// Check connection
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-	else{
-		$fname=$_POST['fname'];
-		$surname=$_POST['Surname'];
-		$MorE=$_POST['MorE'];
-		$newpassword=$_POST['new_password'];
-		$reenter=$_POST['Re-enter_new_password'];
-		if($fname==""||$surname==""||$MorE==""||$newpassword==""||$reenter==""){
-			?>
-				<script type="text/javascript">
-					alert("please fill all the fields.");
-					window.location="index.html";
-				</script>
-			<?php
-		}
-		if($newpassword!=$reenter){
-			?>
-				<script>
-					alert("password mismatched");
-					window.location="index.html";
-				</script>
-			<?php
-
-		}
-
-		if(!mysqli_query($con,"INSERT INTO account (firstname,surname,mobile_or_email,new_password,reenter_password) VALUES ('$fname','$surname','$MorE','$newpassword','$reenter')")){
-			echo("Error description: " . mysqli_error($con));
-		}
-		else{
-			echo "you account has been created successfully";
-			//($_POST['fname'],$_POST['surname'],$_POST['MorE'],$_POST['new_password'],$_POST['Re-enter_new_password'])
-			//'fne','sume','MorE','newword','Re-erd')
-		}
-	}
+        $con=mysqli_connect("localhost","id2166836_sai","chintu.d","id2166836_db");
+	$fname=$_POST['fname'];
+	$surname=$_POST['Surname'];
+	$MorE=$_POST['MorE'];
+	$newpassword=$_POST['new_password'];
+	$reenter=$_POST['Re-enter_new_password'];
+        $date_birth=$_POST['date']."-".$_POST['month']."-".$_POST['year'];
+        //$genders=$_POST['gender'];
+	if($newpassword!=$reenter){
 ?>
-<a href="index.html">Click here Login<a/>
+		<script>
+			alert("password mismatched");
+			window.location="index.html";
+		</script>
+<?php
+
+	}
+
+	if(!mysqli_query($con,"INSERT INTO account (firstname,surname,mobile_or_email,new_password,reenter_password,date_birth) VALUES ('$fname','$surname','$MorE','$newpassword','$reenter','$date_birth')")){
+		echo("Error description: " . mysqli_error($con));
+	}
+	echo "your account has been created successfully";
+	//($_POST['fname'],$_POST['surname'],$_POST['MorE'],$_POST['new_password'],$_POST['Re-enter_new_password'])
+	//'fne','sume','MorE','newword','Re-erd')
+?>
+<a href="/fb">Click here Login<a/>
--- fb_submit.php
+++ fb_submit.php
@@ -1,20 +1,28 @@
 <?php
-	
-	$con=mysqli_connect("localhost","root","sai","chintu");
-// Check connection
-	if (mysqli_connect_errno())
-  	{
-  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  	}
+        $con=mysqli_connect("localhost","id2166836_sai","chintu.d","id2166836_db");
+  	$username=$_POST['username'];
+  	$result =mysqli_query($con,"SELECT * FROM account WHERE mobile_or_email ='$username' ");
 
-  	$result =mysqli_query($con,"SELECT * FROM account");
+  	$row = $result->fetch_assoc();
 
-	while($row = $result->fetch_assoc()) {
+  	if($_POST['password']==$row["new_password"])
+  		echo "Welcome to Facebook";
+  	else{
+ ?>
+ 	<script type="text/javascript">
+ 		alert("Entered email or password is incorrect");
+ 		window.location="/fb";
+ 	</script>
+<?php
+    }
+  	
+
+	/*while($row = $result->fetch_assoc()) {
     	if ($_POST['username']==$row["mobile_or_email"] && $_POST['password']==$row["new_password"]) {
 			echo "Welcome to Facebook";
 
 		}
- 	}
+ 	}*/
  	#echo $row['firstname'];
 	/*if ($_POST['username']==$row['firstname'] && $_POST['password']=='chintu') {
 			echo "you are correct".$_POST['username'];
